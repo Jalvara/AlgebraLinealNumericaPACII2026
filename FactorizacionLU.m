3341488d1ca95c6e05d58dd760f926c70e0fa90e
Jalvara <121142672+Jalvara@users.noreply.github.com>
2026-05-28 11:27:23 -0600

    Add files via upload

---
+++ FactorizacionLU.m
@@ -0,0 +1,14 @@
+function [L,U]=FactorizacionLU(A)
+  n=length(A);
+  L=eye(n);
+  for i=1: n-1
+    L(i+1:n,i)=A(i+1:n,i)/A(i,i);
+    for k=i+1:n
+      A(k,i)=0;
+      for j=i+1:n
+        A(k,j)=A(k,j)-L(k,i)*A(i,j);
+      endfor
+    endfor
+  endfor
+  U=A;
+endfunction
---
+++ FactorizacionLUP.m
@@ -0,0 +1,25 @@
+function [P,L,U]=FactorizacionLUP(A)
+  n=length(A);
+  L=eye(n);
+  P=eye(n);
+  for k=1:n-1
+    [maximo,ik]=max(abs(A(k:n,k)));
+    ik=ik+k-1;
+    v=A(k,:);
+    A(k,:)=A(ik,:);
+    A(ik,:)=v;
+    v=P(k,:);
+    P(k,:)=P(ik,:);
+    P(ik,:)=v;
+    L(k+1:n,k)=A(k+1:n,k)/A(k,k);
+    for j=1:k-1
+      s=L(k,j);
+      L(k,j)=L(ik,j);
+      L(ik,j)=s;
+    endfor
+    for j=k+1:n
+      A(j,:)=A(j,:)-L(j,k)*A(k,:);
+    endfor
+  endfor
+  U=A;
+endfunction
